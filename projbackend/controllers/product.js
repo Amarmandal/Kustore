210c38c48eea7e10e57531019992b80174c08788
amarmandal <amarmandal2073@gmail.com>
2020-09-30 09:13:07 +0545

    Unlimited Products Show features

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -137,7 +137,7 @@ exports.updateProduct = (req, res) => {
 //product listing
 
 exports.getAllProducts = (req, res) => {
-    let limit = req.query.limit ? parseInt(req.query.limit) : 8;
+    let limit = req.query.limit ? parseInt(req.query.limit) : 0;
     let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
 
     Product.find()
